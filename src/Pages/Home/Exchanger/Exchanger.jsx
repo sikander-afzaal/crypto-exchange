d3793f3c041e38aefa99f5bd36f15b67f99ee049
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-12 23:37:02 +0500

    first commit

--- src/Pages/Home/Exchanger/Exchanger.jsx
+++ src/Pages/Home/Exchanger/Exchanger.jsx
@@ -78,7 +78,7 @@ function Exchanger() {
       <h1>WITHOUT A TRACE</h1>
       <div className="row-exchange">
         <p>SEND</p>
-        <p className="rate">${currency * 20}</p>
+        <p className="rate">${Math.floor(currency * 20.82)}</p>
         <p>RECEIVE</p>
       </div>
       <div className="input-div">
