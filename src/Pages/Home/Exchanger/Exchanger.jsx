b1d585eace66324dc7b2e513c8c53ba740353ac5
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-12 16:31:27 +0500

    first commit

--- src/Pages/Home/Exchanger/Exchanger.jsx
+++ src/Pages/Home/Exchanger/Exchanger.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import "./Exchanger.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBitcoin, faEthereum } from "@fortawesome/free-brands-svg-icons";
+import {
+  faBitcoin,
+  faEthereum,
+  faMonero,
+} from "@fortawesome/free-brands-svg-icons";
 import {
   faLongArrowRight,
   faLongArrowLeft,
   faChevronDown,
   faQrcode,
+  faSearch,
+  faLitecoinSign,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
@@ -18,6 +24,8 @@ function Exchanger() {
   const [color, setColor] = useState(false);
   const [value, setValue] = useState("");
   const [value2, setValue2] = useState("");
+  const [search, setSearch] = useState(false);
+  const [search2, setSearch2] = useState(false);
   const handleArrows = () => {
     const original = icon;
     const original2 = icon2;
@@ -30,8 +38,12 @@ function Exchanger() {
     setName2(originalName);
   };
   const handleChange = (e) => {
-    const rx_live = /^[+-]?\d*(?:[.,]\d*)?$/;
-    if (rx_live.test(e.target.value)) {
+    if (search === false) {
+      const rx_live = /^[+-]?\d*(?:[.,]\d*)?$/;
+      if (rx_live.test(e.target.value)) {
+        setValue((prev) => e.target.value);
+      }
+    } else {
       setValue((prev) => e.target.value);
     }
   };
@@ -41,6 +53,24 @@ function Exchanger() {
       setValue2((prev) => e.target.value);
     }
   };
+  const coins = [
+    { img: faBitcoin, name: "BTC" },
+    { img: faEthereum, name: "ETH" },
+    { img: faMonero, name: "XMR" },
+    { img: faLitecoinSign, name: "LTC" },
+    { img: faBitcoin, name: "BTC" },
+    { img: faEthereum, name: "ETH" },
+    { img: faMonero, name: "XMR" },
+    { img: faLitecoinSign, name: "LTC" },
+    { img: faBitcoin, name: "BTC" },
+    { img: faEthereum, name: "ETH" },
+    { img: faMonero, name: "XMR" },
+    { img: faLitecoinSign, name: "LTC" },
+    { img: faBitcoin, name: "BTC" },
+    { img: faEthereum, name: "ETH" },
+    { img: faMonero, name: "XMR" },
+    { img: faLitecoinSign, name: "LTC" },
+  ];
   return (
     <div className="exchanger">
       <h1>WITHOUT A TRACE</h1>
@@ -55,13 +85,50 @@ function Exchanger() {
             <input
               onChange={handleChange}
               value={value}
-              placeholder="0.000"
+              placeholder={` ${search ? "Search Currency" : "0.000"}`}
               type="text"
             />
-            <div className="drop-logo">
-              <FontAwesomeIcon icon={faChevronDown} />
-              <p>{name}</p>
-              <FontAwesomeIcon icon={icon} />
+            {search ? (
+              <div
+                onClick={() => {
+                  setSearch((prev) => !prev);
+                  setValue("");
+                }}
+                className="drop-logo"
+              >
+                <FontAwesomeIcon icon={faSearch} />
+                <FontAwesomeIcon icon={faChevronDown} />
+              </div>
+            ) : (
+              <div
+                onClick={() => {
+                  setSearch((prev) => !prev);
+                  setValue("");
+                }}
+                className="drop-logo"
+              >
+                <FontAwesomeIcon icon={faChevronDown} />
+                <p>{name}</p>
+                <FontAwesomeIcon icon={icon} />
+              </div>
+            )}
+            <div className={`dropdown ${search ? "open-drop" : ""}`}>
+              {coins.map((elem, index) => {
+                return (
+                  <div
+                    onClick={() => {
+                      setSearch(false);
+                      setName(elem.name);
+                      setIcon(elem.img);
+                    }}
+                    className="dropdown-row"
+                    key={index}
+                  >
+                    <FontAwesomeIcon icon={elem.img} />
+                    <p>{elem.name}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div className="desc-input">
@@ -83,17 +150,54 @@ function Exchanger() {
         </div>
         <div className="input1 input2">
           <div className={`inputs ${color ? "orange" : ""}`}>
-            <div className="drop-logo">
-              <FontAwesomeIcon icon={icon2} />
-              <p>{name2}</p>
-              <FontAwesomeIcon icon={faChevronDown} />
-            </div>
+            {search2 ? (
+              <div
+                onClick={() => {
+                  setSearch2((prev) => !prev);
+                  setValue2("");
+                }}
+                className="drop-logo"
+              >
+                <FontAwesomeIcon icon={faSearch} />
+                <FontAwesomeIcon icon={faChevronDown} />
+              </div>
+            ) : (
+              <div
+                onClick={() => {
+                  setSearch2((prev) => !prev);
+                  setValue2("");
+                }}
+                className="drop-logo"
+              >
+                <FontAwesomeIcon icon={icon2} />
+                <p>{name2}</p>
+                <FontAwesomeIcon icon={faChevronDown} />
+              </div>
+            )}
             <input
               onChange={handleChange2}
               value={value2}
-              placeholder="0.000"
+              placeholder={` ${search2 ? "Search Currency" : "0.000"}`}
               type="text"
             />
+            <div className={`dropdown ${search2 ? "open-drop" : ""}`}>
+              {coins.map((elem, index) => {
+                return (
+                  <div
+                    onClick={() => {
+                      setSearch2(false);
+                      setName2(elem.name);
+                      setIcon2(elem.img);
+                    }}
+                    className="dropdown-row"
+                    key={index}
+                  >
+                    <FontAwesomeIcon icon={elem.img} />
+                    <p>{elem.name}</p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
           <div className="desc-input">
             <p>
