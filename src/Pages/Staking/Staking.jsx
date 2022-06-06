3c59da7a51c12543675c8d69428e37820ebfa759
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-06-06 19:12:58 +0500

    commit

--- src/Pages/Staking/Staking.jsx
+++ src/Pages/Staking/Staking.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Staking.css";
+import connect from "../../Assets/connect.svg";
+import meta from "../../Assets/meta.svg";
 function Staking() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="staking">
+      <div
+        onClick={() => setOpen((prev) => !prev)}
+        className={`overlay ${open ? "display-flex" : ""}`}
+      ></div>
+      <div className={`pop-up ${open ? "display-flex" : ""}`}>
+        <div className="pop">
+          <img src={meta} alt="" />
+          <h2>MetaMask</h2>
+        </div>
+        <div className="pop">
+          <img src={connect} alt="" />
+          <h2>Wallet Connect</h2>
+        </div>
+      </div>
+      <button onClick={() => setOpen(true)} className="connect">
+        Connect
+      </button>
       <div className="top-staking">
         <h1>Stake $POOF</h1>
         <p>
@@ -60,6 +80,25 @@ function Staking() {
           <p>13/06/2022, 18:35:52</p>
         </div>
         <button className="approve">Approve</button>
+      </div>
+      <div className="bottom-staking">
+        <div className="staking-box-div">
+          <div className="stake-box">
+            <h2>$DREAM STAKED</h2>
+            <p>0.00</p>
+          </div>
+          <div className="stake-box">
+            <h2>$DREAM EARNINGS</h2>
+            <p>0.00</p>
+          </div>
+        </div>
+        <input
+          disabled={true}
+          type="number"
+          className="staking-input"
+          placeholder="Your Staked Holdings (0)"
+        />
+        <div className="approve">REDEEM</div>
       </div>
     </div>
   );
